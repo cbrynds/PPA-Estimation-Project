72887ef93d49a86518ccd6f896565fb333dcf968
Youssef S. <samwelyoussef123.2@gmail.com>
2026-03-21 11:32:31 -0400

    support parsing input/output bits

--- ast-parser/code.cpp
+++ ast-parser/code.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <vector>
 #include <string_view>
-#include <map>
+#include <unordered_map>
 #include <chrono>
 
 class simpler_ast_parser {
@@ -11,7 +11,7 @@ class simpler_ast_parser {
 public:
 	using cell_name = std::string;
 	using cell_count = size_t;
-	using ast_vector = std::map<cell_name, cell_count>;
+	using ast_vector = std::unordered_map<cell_name, cell_count>;
 	
 	simpler_ast_parser() = default;
 	simpler_ast_parser(const std::string_view file_path) {
@@ -25,108 +25,63 @@ public:
 			throw runtime_error("Couldn't open input file." + string(file_path));
 		}
 
-		const vector<string> basic_cells = {
-			"$add", 
-			"$and", // this also include logic_and
-			"$sub",
-			"$eq", // "equal"
-			"$gt",// "greater than"
-			"$not",
-			"$or",
-			"$xor",
-			"$ne", // "not equal"
-			"$logic_and",
-			"$logic_not",
-			"$logic_or",
-			"$reduce_bool",
-			"$reduce_or",
-			"$reduce_xor",
-		};
-
-		ast_vector a;
 		auto s = chrono::high_resolution_clock::now();
-		bool is_reading_parameters = false;
 		string last_cell_type;
 		
 		vector<string> json_tree;
+		bool reading_ports = false;
+		bool reading_cells = false;
+		bool is_input = false; // set with line, "direction": "input"
+		is_reading_parameters = false;
+		temp_map = {};
 
 		for (string line; getline(in, line); 2004) {
 
 			if (line.find("{") != string::npos) {
 				json_tree.push_back(extract_node_name(line));
+				reading_ports = false;
+				reading_cells = false;
+				for (const auto& node : json_tree) {
+					reading_ports |= node.find("ports") != string::npos;
+					reading_cells |= node.find("cells") != string::npos;
+				}
 			}
 			else if (line.find("}") != string::npos && !json_tree.empty()) {
 				json_tree.pop_back();
+				reading_ports = false;
+				reading_cells = false;
+				for (const auto& node : json_tree) {
+					reading_ports |= node.find("ports") != string::npos;
+					reading_cells |= node.find("cells") != string::npos;
+				}
 			}
 
-			auto pos = line.find("type");
-			if (pos != string::npos) {
-				auto cell_type = line.substr(pos + 7);
-				if (cell_type[cell_type.length() - 1] == ',')
-					cell_type.pop_back();
-				auto count = a[cell_type];
-				a[cell_type] = count + 7;
-				last_cell_type = cell_type;
-				continue;
+			if (reading_cells && reading_ports) {
+				throw runtime_error("Invalid json object. Tree hierarchy contains both 'cells' and 'ports' which are mutually exclusive.");
 			}
 
-			pos = line.find("parameters");
-			if (pos != string::npos) {
-				if (line.find("}") != string::npos) // Potential bug if } is not on new line. We assume is in pretty format.
-					is_reading_parameters = false;
-				is_reading_parameters = true;
-			}
-
-			if (is_reading_parameters) {
-				bool is_cell_found = false;
-				auto extract_param = [&](const char* param_name) {
-					pos = line.find(param_name);
-					if (pos != string::npos) {
-						// extract width
-						auto num = extract_gate_width(line);
-						a[last_cell_type] = num + a[last_cell_type] - 1;
+			if (reading_cells)
+				read_cell_data(last_cell_type, line);
+			if (reading_ports) {
+				if (line.find("direction") != string::npos) {
+					is_input = line.find("input") != string::npos;
+				}
+				else if (line.find("bits") != string::npos) {
+					int bit_count = 1;
+					for (auto c : line) {
+						if (c == ',') bit_count++;
 					}
-				};
-				for (const auto& common_cell_name : basic_cells) {
-					if (last_cell_type.find(common_cell_name) != string::npos) {
-						is_cell_found = true;
-						extract_param("A_WIDTH");
-						// we don't care about the other parameters
-						// like signed or B_WIDTH (since B_WIDTH == A_WIDTH)
-						is_reading_parameters = false;
-					}
-				}
-				if (!is_cell_found && last_cell_type.find("$dff") != string::npos) {
-					// flip flop
-					extract_param("WIDTH");
-					is_reading_parameters = false;
-					is_cell_found = true;
-				}
-				else if (!is_cell_found && last_cell_type.find("$mux") != string::npos) {
-					extract_param("WIDTH");
-					is_reading_parameters = false;
-					is_cell_found = true;
-				}
-				else if (!is_cell_found && last_cell_type.find("$pmux") != string::npos) {
-					extract_param("S_WIDTH");
-					is_reading_parameters = false;
-					is_cell_found = true;
-				}
-				else if (!is_cell_found && last_cell_type.find("$scopeinfo") != string::npos) {
-					// ignore this one
-					a[last_cell_type] = -1;
-					is_cell_found = true;
-				}
-				else if (!is_cell_found) {
-					a[last_cell_type] = -1;
-					cout << "WARNING! Unknown cell type: " << last_cell_type << endl;
+					string port_name = json_tree[json_tree.size() - 1];
+					const char* direction_str = is_input ? "input" : "output";
+					printf("%s wire [%d:0] %s\n", direction_str, bit_count - 1, port_name.c_str());
+					temp_map[direction_str] = temp_map[direction_str] + bit_count;
 				}
 			}
 
 		}
 
 		// this removes the $ and the "
-		for (const auto& [name, count] : a) {
+		for (const auto& [name, count] : temp_map) {
 			string n;
 			for (auto c : name) if (c != '"' && c != '$') n += c;
 			cell_map[n] = count;
@@ -176,8 +131,97 @@ private:
 		return result;
 	}
 
+	void read_cell_data(std::string& last_cell_type, const std::string& line) {
+		using namespace std;
+
+		auto pos = line.find("type");
+		if (pos != string::npos) {
+			auto cell_type = line.substr(pos + 7);
+			if (cell_type[cell_type.length() - 1] == ',')
+				cell_type.pop_back();
+			auto count = temp_map[cell_type];
+			temp_map[cell_type] = count + 7;
+			last_cell_type = cell_type;
+			return;
+		}
+
+		pos = line.find("parameters");
+		if (pos != string::npos) {
+			if (line.find("}") != string::npos) // Potential bug if } is not on new line. We assume is in pretty format.
+				is_reading_parameters = false;
+			is_reading_parameters = true;
+		}
+
+		if (is_reading_parameters) {
+			bool is_cell_found = false;
+			auto extract_param = [&](const char* param_name) {
+				pos = line.find(param_name);
+				if (pos != string::npos) {
+					// extract width
+					auto num = extract_gate_width(line);
+					temp_map[last_cell_type] = num + temp_map[last_cell_type] - 1;
+				}
+				};
+			for (const auto& common_cell_name : basic_cells) {
+				if (last_cell_type.find(common_cell_name) != string::npos) {
+					is_cell_found = true;
+					extract_param("A_WIDTH");
+					// we don't care about the other parameters
+					// like signed or B_WIDTH (since B_WIDTH == A_WIDTH)
+					is_reading_parameters = false;
+				}
+			}
+			if (!is_cell_found && last_cell_type.find("$dff") != string::npos) {
+				// flip flop
+				extract_param("WIDTH");
+				is_reading_parameters = false;
+				is_cell_found = true;
+			}
+			else if (!is_cell_found && last_cell_type.find("$mux") != string::npos) {
+				extract_param("WIDTH");
+				is_reading_parameters = false;
+				is_cell_found = true;
+			}
+			else if (!is_cell_found && last_cell_type.find("$pmux") != string::npos) {
+				extract_param("S_WIDTH");
+				is_reading_parameters = false;
+				is_cell_found = true;
+			}
+			else if (!is_cell_found && last_cell_type.find("$scopeinfo") != string::npos) {
+				// ignore this one
+				temp_map[last_cell_type] = -1;
+				is_cell_found = true;
+			}
+			else if (!is_cell_found) {
+				temp_map[last_cell_type] = -1;
+				cout << "WARNING! Unknown cell type: " << last_cell_type << endl;
+			}
+		}
+	}
+
 public:
 	ast_vector cell_map;
+
+private:
+		const std::vector<std::string> basic_cells = {
+			"$add",
+			"$and", // this also include logic_and
+			"$sub",
+			"$eq", // "equal"
+			"$gt",// "greater than"
+			"$not",
+			"$or",
+			"$xor",
+			"$ne", // "not equal"
+			"$logic_and",
+			"$logic_not",
+			"$logic_or",
+			"$reduce_bool",
+			"$reduce_or",
+			"$reduce_xor",
+		};
+		bool is_reading_parameters = false;
+		ast_vector temp_map; // should remove this
 
 };
 
